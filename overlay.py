5712cfb18149b9bd6bb05ed28f5863f27263caf3
Takushi Miyoshi <tmiyoshi@nidcd.nih.gov>
2020-02-06 14:50:58 -0500

    Bug fix in handling TZCYX images

--- overlay.py
+++ overlay.py
@@ -42,7 +42,7 @@ shift_x, shift_y = args.image_shift
 if args.output_file is None:
     output_filename = pathlib.Path(input_filename).stem.split('.')[0] + '_out.tif'
     if input_filename == output_filename:
-        raise Exception('input_filename == output_filename')
+        raise Exception('input_filename == output_filename.')
 else:
     output_filename = args.output_file[0]
 
@@ -52,13 +52,15 @@ with tifffile.TiffFile(input_filename) as tiff:
     axes = tiff.series[0].axes
     image = tiff.asarray(series = 0)
     print('Found axis:', axes)
-    print('Image shape', image.shape)
+    if 'S' in axes:
+        raise Exception('Color images are not supported.')
     if 'T' in axes:
         total_time = len(image)
-        orig_image = numpy.split(image, len(image)) # becomes a list of [1, height, width]
+        orig_image = [x[0] for x in numpy.split(image, len(image))] # remove the first axis [1, (frame,) height, width]
     else:
         total_time = 1
         orig_image = [image]
+    print('Image shape:', total_time, ' x ', image.shape)
 
 if 'Z' not in axes:
     print('Adding temporary Z axis.')
@@ -67,31 +69,39 @@ if 'Z' not in axes:
 if 'C' in axes:
     print('Using the first channel only.')
     for index in range(len(orig_image)):
-        orig_image[index] = orig_image[index][..., 0:1, :, :]
+        orig_image[index] = orig_image[index][:, 0:1, :, :]
+else:
+    print('Adding temporary C axis.')
+    for index in range(len(orig_image)):
+        orig_image[index] = orig_image[index][:, numpy.newaxis, :, :]
 
-orig_image = numpy.asarray(orig_image)
-print('Original image was shaped into: ', orig_image.shape)
+print('Original image was shaped into: ', orig_image[0].shape)
 
 # overlay dimensions should be in TZCYXS order
-total_frame = orig_image.shape[1]
+total_frame = orig_image[0].shape[0]
+split_shift = orig_image[0].shape[-1] // 2
+total_width = orig_image[0].shape[-1]
 if crop_height is None:
-    crop_height = orig_image.shape[-2] - crop_y
+    crop_height = orig_image[0].shape[-2] - crop_y
 if crop_width is None:
-    crop_width = orig_image.shape[-1] // 2 - crop_x
+    crop_width = orig_image[0].shape[-1] // 2 - crop_x
+shift_array = (0, shift_y, shift_x)
 
-output_image = numpy.zeros((total_time, total_frame, 2, crop_height, crop_width), dtype = orig_image.dtype)
-print('Output image was shaped into: ', output_image.shape)
-output_image[:, :, 1, :, :] = orig_image[:, :, 0, crop_y:(crop_y + crop_height), crop_x:(crop_x + crop_width)]
+output_image = []
+for index in range(len(orig_image)):
+    image = numpy.zeros((total_frame, 2, crop_height, crop_width), dtype = orig_image[0].dtype)
+    image[:, 1, :, :] = orig_image[index][:, 0, crop_y:(crop_y + crop_height), crop_x:(crop_x + crop_width)]
 
-split_shift = orig_image.shape[-1] // 2
-shift_array = (0, 0, 0, shift_y, shift_x)
-paste_image = orig_image[:, :, :, crop_y:(crop_y + crop_height), (split_shift + crop_x):(split_shift + crop_x + crop_width)]
-paste_image = shift(paste_image, shift_array)
-output_image[:, :, 0, :, :] = paste_image
-print('Pasting image was shaped by', shift_array)
+    paste_image = shift(orig_image[index][:, 0, :, split_shift:total_width], shift_array)
+    image[:, 0, :, :] = paste_image[:, crop_y:(crop_y + crop_height), crop_x:(crop_x + crop_width)]
+
+    output_image.append(image)
+    print('Index: ', index, 'Shape: ', image.shape, 'Shift: ', shift_array)
 
 # output ImageJ, dimensions should be in TZCYXS order
 #output_image = numpy.array(output_image).transpose(1, 0, 2, 3)
+output_image = numpy.array(output_image)
+print('Output image was shaped into: ',output_image.shape)
 tifffile.imsave(output_filename, output_image, imagej = True, \
                 resolution = (1 / xy_resolution, 1 / xy_resolution), \
                 metadata = {'spacing': z_spacing, 'unit': 'um', 'Composite mode': 'composite'})
