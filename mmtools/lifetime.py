537f81e48229dc8622cded49b1547134a662f325
Takushi Miyoshi <tmiyoshi@nih.gov>
2020-10-05 13:46:23 -0400

    Add a column for simulated regression

--- mmtools/lifetime.py
+++ mmtools/lifetime.py
@@ -4,7 +4,7 @@ import sys, numpy, pandas, time
 
 class Lifetime:
     def __init__ (self, spot_table, time_scale = 1.0):
-        self.lifetime_columns = ['life_count', 'life_time', 'spot_count']
+        self.lifetime_columns = ['life_count', 'life_time', 'spot_count', 'spot_regression']
         self.time_scale = time_scale
         self.spot_table = spot_table
 
@@ -55,12 +55,14 @@ class Lifetime:
         output_indexes = [i for i in range(1, lifecount_max + 1)]
         output_times = [i * self.time_scale for i in output_indexes]
         output_counts = [len(work_table[work_table.life_count == i]) for i in output_indexes]
+        output_regs = [len(work_table[work_table.life_count >= i]) for i in output_indexes]
 
         # output data
         output_table = pandas.DataFrame({ \
                             self.lifetime_columns[0] : output_indexes, \
                             self.lifetime_columns[1] : output_times, \
-                            self.lifetime_columns[2] : output_counts}, \
+                            self.lifetime_columns[2] : output_counts, \
+                            self.lifetime_columns[3] : output_regs}, \
                             columns = self.lifetime_columns)
 
         return output_table
