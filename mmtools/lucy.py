4082ca861accaaf582d782d80a9491cabcd63a3b
Takushi Miyoshi <tmiyoshi@nidcd.nih.gov>
2021-02-23 20:27:23 -0500

    Changed to save calculated psf_fft

--- mmtools/lucy.py
+++ mmtools/lucy.py
@@ -5,7 +5,6 @@ from scipy.signal import convolve
 from scipy.ndimage import zoom
 from scipy.ndimage.interpolation import shift
 import cupy
-#from cupyx.scipy.ndimage import shift as cupyx_shift
 #from cupyx.scipy.ndimage import zoom as cupyx_zoom
 #from cupyx.scipy.ndimage import convolve as cupyx_convolve
 
@@ -23,6 +22,17 @@ class Lucy:
             print("Turning on GPU: {0}, PCI-bus ID: {1}".format(self.gpu_id, device.pci_bus_id))
             if self.use_fft is False:
                 print("GPU version of the Matrix deconvolution is distabled. FFT used.")
+
+    def update_psf_fft (self, shape):
+        if (self.psf_fft is None) or (self.psf_fft.shape != shape):
+            psf_resized = numpy.zeros(shape, dtype = numpy.float)
+            limits = numpy.minimum(shape, self.psf.shape)
+            shifts = (numpy.array(shape) - numpy.array(self.psf.shape)) // 2
+            psf_resized[0:limits[0], 0:limits[1], 0:limits[2]] = self.psf[0:limits[0], 0:limits[1], 0:limits[2]]
+            psf_resized = shift(psf_resized, shifts)
+
+            self.psf_fft = numpy.fft.fftn(numpy.fft.ifftshift(psf_resized))
+            self.hat_fft = numpy.fft.fftn(numpy.fft.ifftshift(numpy.flip(psf_resized)))
 
     def deconvolve (self, input_image, iterations = 10, z_zoom = 1, zoom_result = False):
         if input_image.ndim < 3:
@@ -42,13 +52,6 @@ class Lucy:
                 latent_est = self.deconvolve_gpu_fft(input_image, iterations, z_zoom, zoom_result)
 
         return latent_est
-
-    def resized_psf (self, shape):
-        psf_resized = numpy.zeros(shape, dtype = self.psf.dtype)
-        limits = numpy.minimum(shape, self.psf.shape)
-        shifts = (numpy.array(shape) - numpy.array(self.psf.shape)) // 2
-        psf_resized[0:limits[0], 0:limits[1], 0:limits[2]] = self.psf[0:limits[0], 0:limits[1], 0:limits[2]]
-        return shift(psf_resized, shifts)
 
     def deconvolve_cpu_mat (self, input_image, iterations = 10, z_zoom = 1, zoom_result = False):
         orig_image = input_image.astype(numpy.float)
@@ -72,11 +75,7 @@ class Lucy:
             # zoom image, may be zoomed when z_zoom = 1.0 (float)
             orig_image = zoom(orig_image, (z_zoom, 1.0, 1.0))
 
-        if (self.psf_fft is None) or (self.psf_fft.shape != orig_image.shape):
-            psf_resized = self.resized_psf(orig_image.shape)
-            self.psf_fft = numpy.fft.fftn(numpy.fft.ifftshift(psf_resized))
-            self.hat_fft = numpy.fft.fftn(numpy.fft.ifftshift(numpy.flip(psf_resized)))
-
+        self.update_psf_fft(orig_image.shape)
         latent_est = orig_image.copy()
         for i in range(iterations):
             ratio = orig_image / numpy.abs(numpy.fft.ifftn(self.psf_fft * numpy.fft.fftn(latent_est)))
@@ -103,23 +102,14 @@ class Lucy:
             print("Iteration:", i)
             print(latent_est_gpu.shape)
             print(temp_image_gpu.shape)
-            #cupyx_convolve(latent_est_gpu, self.psf_image_gpu, output = temp_image_gpu, mode = "constant")
             temp_image_gpu = cupyx_convolve(latent_est_gpu, self.psf_image_gpu, mode = "constant")
-
-            temp = cupy.asnumpy(temp_image_gpu)
-            print(min(temp))
             print("Convolution 1")
-
-            #temp_image_gpu = orig_image_gpu / temp_image_gpu
-            temp_image_gpu = cupy.divide(orig_image_gpu, temp_image_gpu)
+            temp_image_gpu = orig_image_gpu / temp_image_gpu
             print("Division")
-
             cupyx_convolve(latent_est_gpu, self.psf_hat_gpu, output = temp_image_gpu, mode = "constant")
             print("Convolution 2")
-
             latent_est_gpu = latent_est_gpu * temp_image_gpu
             print("Done")
-
             #latent_est_gpu = latent_est_gpu * \
             #    cupyx_convolve(orig_image_gpu / cupyx_convolve(latent_est_gpu, self.psf_image_gpu, \
             #                                                  mode = "constant"), \
@@ -136,13 +126,15 @@ class Lucy:
             # zoom image, may be zoomed when z_zoom = 1.0 (float)
             orig_image = zoom(orig_image, (z_zoom, 1.0, 1.0))
 
-        if (self.psf_fft is None) or (self.psf_fft.shape != orig_image.shape):
-            psf_resized = self.resized_psf(orig_image.shape).astype(numpy.float32)
-            self.psf_fft = numpy.fft.fftn(numpy.fft.ifftshift(psf_resized))
-            self.hat_fft = numpy.fft.fftn(numpy.fft.ifftshift(numpy.flip(psf_resized)))
-        
-        psf_fft = cupy.array(self.psf_fft)
-        hat_fft = cupy.array(self.hat_fft)
+        psf_resized = numpy.zeros_like(orig_image, dtype = numpy.float)
+        limits = numpy.minimum(orig_image.shape, self.psf.shape)
+        shifts = (numpy.array(orig_image.shape) - numpy.array(self.psf.shape)) // 2
+        psf_resized[0:limits[0], 0:limits[1], 0:limits[2]] = self.psf[0:limits[0], 0:limits[1], 0:limits[2]]
+        psf_resized = shift(psf_resized, shifts)
+        psf_resized = cupy.array(psf_resized.astype(numpy.float32))
+
+        psf_fft = cupy.fft.fftn(cupy.fft.ifftshift(psf_resized))
+        hat_fft = cupy.fft.fftn(cupy.fft.ifftshift(cupy.flip(psf_resized)))
 
         orig_image = cupy.array(orig_image)
         latent_est = orig_image.copy()
