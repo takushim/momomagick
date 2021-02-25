42aa0c8879d3ec151590b5a56949dbdeecd2b9e0
Takushi Miyoshi <takushi.miyoshi@nih.gov>
2021-02-25 13:50:21 -0500

    Used module variables to store cupy modules and functions

--- mmtools/lucy.py
+++ mmtools/lucy.py
@@ -5,6 +5,11 @@ from scipy.signal import convolve
 from scipy.ndimage import zoom
 from . import mmtiff
 
+# global variables to store modules and functions
+g_cupy = None
+g_cupyx_zoom = None
+g_cupyx_convolve = None
+
 class Lucy:
     def __init__ (self, psf_image, gpu_id = None, use_fft = False, save_memory = False):
         self.psf = psf_image.astype(numpy.float) / numpy.sum(psf_image)
@@ -26,11 +31,15 @@ class Lucy:
             import cupy
             from cupyx.scipy.ndimage import zoom as cupyx_zoom
             from cupyx.scipy.signal import convolve as cupyx_convolve
-            self.cupy = cupy
-            self.cupyx_zoom = cupyx_zoom
-            self.cupyx_convolve = cupyx_convolve
+            # export modules to the global scope
+            global g_cupy
+            global g_cupyx_zoom
+            global g_cupyx_convolve
+            g_cupy = cupy
+            g_cupyx_zoom = cupyx_zoom
+            g_cupyx_convolve = cupyx_convolve
 
-            device = self.cupy.cuda.Device(gpu_id)
+            device = g_cupy.cuda.Device(gpu_id)
             device.use()
             print("Turning on GPU: {0}, PCI-bus ID: {1}".format(self.gpu_id, device.pci_bus_id))
             if self.save_memory is False:
@@ -51,17 +60,17 @@ class Lucy:
         if self.gpu_id is not None:
             # send psf images to the GPU
             if self.save_memory:
-                psf_resized = self.cupy.array(psf_resized.astype(numpy.float32))
+                psf_resized = g_cupy.array(psf_resized.astype(numpy.float32))
             else:
-                psf_resized = self.cupy.array(psf_resized)
+                psf_resized = g_cupy.array(psf_resized)
 
             # cupy seems to output complex64 for float32 input
-            self.psf_fft_gpu = self.cupy.fft.fftn(self.cupy.fft.ifftshift(psf_resized))
-            self.hat_fft_gpu = self.cupy.fft.fftn(self.cupy.fft.ifftshift(self.cupy.flip(psf_resized)))
+            self.psf_fft_gpu = g_cupy.fft.fftn(g_cupy.fft.ifftshift(psf_resized))
+            self.hat_fft_gpu = g_cupy.fft.fftn(g_cupy.fft.ifftshift(g_cupy.flip(psf_resized)))
 
             # copy to cpu
-            self.psf_fft = self.cupy.asnumpy(self.psf_fft_gpu)
-            self.hat_fft = self.cupy.asnumpy(self.hat_fft_gpu)
+            self.psf_fft = g_cupy.asnumpy(self.psf_fft_gpu)
+            self.hat_fft = g_cupy.asnumpy(self.hat_fft_gpu)
         else:
             if self.save_memory:
                 print("Preparing complex64 psf to save memory.")
@@ -114,50 +123,50 @@ class Lucy:
 
     def deconvolve_gpu_mat (self, input_image, iterations = 10, z_scale = 1, scale_result = False):
         if self.save_memory:
-            orig_image = self.cupy.array(input_image.astype(numpy.float32))
-            psf_image = self.cupy.array(self.psf.astype(numpy.float32))
-            hat_image = self.cupy.array(self.hat.astype(numpy.float32))
+            orig_image = g_cupy.array(input_image.astype(numpy.float32))
+            psf_image = g_cupy.array(self.psf.astype(numpy.float32))
+            hat_image = g_cupy.array(self.hat.astype(numpy.float32))
         else:
-            orig_image = self.cupy.array(input_image.astype(numpy.float))
-            psf_image = self.cupy.array(self.psf)
-            hat_image = self.cupy.array(self.hat)
+            orig_image = g_cupy.array(input_image.astype(numpy.float))
+            psf_image = g_cupy.array(self.psf)
+            hat_image = g_cupy.array(self.hat)
 
         # scale image, may be scaled when z_scale = 1.0 (float)
         if z_scale != 1:
-            orig_image = self.cupyx_zoom(orig_image, (z_scale, 1.0, 1.0), order = 1)
+            orig_image = g_cupyx_zoom(orig_image, (z_scale, 1.0, 1.0), order = 1)
 
         latent_est = orig_image.copy()
         for i in range(iterations):
             latent_est = latent_est * \
-                self.cupyx_convolve(orig_image / self.cupyx_convolve(latent_est, psf_image, "same"), \
-                                    hat_image, "same")
+                g_cupyx_convolve(orig_image / g_cupyx_convolve(latent_est, psf_image, "same"), \
+                                 hat_image, "same")
 
         if scale_result and z_scale != 1:
-            latent_est = self.cupyx_zoom(latent_est, (1.0/z_scale, 1.0, 1.0), order = 1)
+            latent_est = g_cupyx_zoom(latent_est, (1.0/z_scale, 1.0, 1.0), order = 1)
 
-        return self.cupy.asnumpy(latent_est)
+        return g_cupy.asnumpy(latent_est)
 
     def deconvolve_gpu_fft (self, input_image, iterations = 10, z_scale = 1, scale_result = False):
         if self.save_memory:
-            orig_image = self.cupy.array(input_image.astype(numpy.float32))
+            orig_image = g_cupy.array(input_image.astype(numpy.float32))
         else:
-            orig_image = self.cupy.array(input_image.astype(numpy.float))
+            orig_image = g_cupy.array(input_image.astype(numpy.float))
 
         # scale image, may be scaled when z_scale = 1.0 (float)
         if z_scale != 1:
-            orig_image = self.cupyx_zoom(orig_image, (z_scale, 1.0, 1.0), order = 1)
+            orig_image = g_cupyx_zoom(orig_image, (z_scale, 1.0, 1.0), order = 1)
 
         self.update_psf_fft(orig_image.shape)
 
         #orig_image = cupy.array(orig_image)
         latent_est = orig_image.copy()
         for i in range(iterations):
-            ratio = orig_image / self.cupy.abs(self.cupy.fft.ifftn(self.psf_fft_gpu * self.cupy.fft.fftn(latent_est)))
-            latent_est = latent_est * self.cupy.abs(self.cupy.fft.ifftn(self.hat_fft_gpu * self.cupy.fft.fftn(ratio)))
+            ratio = orig_image / g_cupy.abs(g_cupy.fft.ifftn(self.psf_fft_gpu * g_cupy.fft.fftn(latent_est)))
+            latent_est = latent_est * g_cupy.abs(g_cupy.fft.ifftn(self.hat_fft_gpu * g_cupy.fft.fftn(ratio)))
 
         #latent_est = cupy.asnumpy(latent_est)
         if scale_result and z_scale != 1:
-            latent_est = self.cupyx_zoom(latent_est, (1.0/z_scale, 1.0, 1.0), order = 1)
+            latent_est = g_cupyx_zoom(latent_est, (1.0/z_scale, 1.0, 1.0), order = 1)
 
-        return self.cupy.asnumpy(latent_est)
+        return g_cupy.asnumpy(latent_est)
 
