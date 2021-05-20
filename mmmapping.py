f3b2e80c11e39bac061ff6d7774528f4d1cc6ca9
Takushi Miyoshi <takushi.miyoshi@nih.gov>
2021-05-20 17:12:24 -0400

    Suport CZI files

--- mmmapping.py
+++ mmmapping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 
-import sys, pathlib, re, argparse, json, numpy, pandas
+import sys, pathlib, re, argparse, json, numpy, pandas, czifile
+import xml.etree.ElementTree as ET
 from matplotlib import pyplot
 
 # default values
-input_folders = None
+input_locations = None
 output_prefix = 'Mapping'
 suffix_tsv_file = ".txt"
 suffix_graph_file = ".png"
+axis_mode = 'auto'
+
+# check if the data locations can be treated
+def acceptable (location):
+    path = pathlib.Path(location)
+    if not path.exists():
+        return False
+    elif path.is_dir():
+        if path.glob("*_metadata.txt"):
+            return True
+        else:
+            return False
+    elif path.suffix.lower() == ".czi":
+        return True
+
+    return False
 
 # parse arguments
 parser = argparse.ArgumentParser(description='Map the localization of cells', \
@@ -16,42 +33,58 @@ parser = argparse.ArgumentParser(description='Map the localization of cells', \
 parser.add_argument('-o', '--output-prefix', default=output_prefix, \
                     help='prefix of output files')
 
-parser.add_argument('input_folders', nargs = '*', default=input_folders, \
-                    help='folders to process (fetch all for None)')
+parser.add_argument('-a', '--axis-mode', default=axis_mode, choices = ['auto', 'dispim', 'airy'], \
+                    help='set the directions of axis')
+
+parser.add_argument('input_locations', nargs = '*', default=input_locations, \
+                    help='folders or czi files to process (fetch all for None)')
 args = parser.parse_args()
 
 # set arguments
 output_prefix = args.output_prefix
-input_folders = args.input_folders
+input_locations = args.input_locations
+axis_mode = args.axis_mode
 
 output_tsv_filename = output_prefix + suffix_tsv_file
 output_graph_filename = output_prefix + suffix_graph_file
 
-
-if input_folders is None or len(input_folders) == 0:
-    input_folders = filter(lambda x: pathlib.Path(x).is_dir(), pathlib.Path(".").glob("*"))
-    input_folders = [str(x) for x in input_folders]
+if input_locations is None or len(input_locations) == 0:
+    input_locations = list(filter(lambda x: acceptable(x), pathlib.Path(".").glob("*")))
 else:
-    temp_folders = []
-    for input_folder in input_folders:
-        temp_folders.extend(list(pathlib.Path(".").glob(input_folder)))
-    input_folders = [str(x) for x in filter(lambda x: x.is_dir() and x.exists(), temp_folders)]
+    locations = []
+    for input_location in input_locations:
+        locations.extend(list(pathlib.Path(".").glob(input_location)))
+    input_locations = list(filter(lambda x: acceptable(x), locations))
 
-if len(input_folders) == 0:
+if len(input_locations) == 0:
     print("No input folders. Exit.")
     sys.exit()
 
 positions = []
-for index, input_folder in enumerate(input_folders):
-    metafile = [str(x) for x in pathlib.Path(input_folder).glob("*_metadata.txt")][0]
-    with open(metafile, 'r') as file:
-        data = json.load(file)
-        pattern = re.compile(r'[-+]?[0-9,]*\.?[0-9,]+([eE][-+]?[0-9]+)?')
-        pos_x = float(pattern.match(data["Summary"]["Position_X"]).group().replace(',', ''))
-        pos_y = float(pattern.match(data["Summary"]["Position_Y"]).group().replace(',', ''))
-    positions.append({'index': index, 'name': input_folder, 'x': pos_x, 'y': pos_y})
+for index, input_location in enumerate(input_locations):
+    if input_location.is_dir():
+        metafile = next(pathlib.Path(input_location).glob("*_metadata.txt"))
+        with metafile.open('r', encoding = 'iso-8859-1') as file:
+            data = json.load(file)
+            pattern = re.compile(r'[-+]?[0-9,]*\.?[0-9,]+([eE][-+]?[0-9]+)?')
+            pos_x = float(pattern.match(data["Summary"]["Position_X"]).group().replace(',', ''))
+            pos_y = float(pattern.match(data["Summary"]["Position_Y"]).group().replace(',', ''))
+    elif input_location.suffix.lower() == ".czi":
+        with czifile.CziFile(str(input_location)) as file:
+            root = ET.fromstring(file.metadata())
+            element = root.find('./Metadata/Information/Image/Dimensions/S/Scenes/Scene[@Index="0"]/Positions/Position')
+            pos_x = float(element.attrib['X'])
+            pos_y = float(element.attrib['Y'])
+    else:
+        print("Ignoring:", str(input_location))
+        continue
+    
+    positions.append({'index': index, 'name': input_location, 'x': pos_x, 'y': pos_y})
 
 pos_table = pandas.DataFrame(positions)
+if len(pos_table) == 0:
+    print("Empty table. No output.")
+    sys.exit()
 
 print("Output TSV file:", output_tsv_filename)
 print(pos_table)
@@ -64,14 +97,40 @@ axes.set_title("Locations of samples", size = 'xx-large')
 axes.set_xlabel("X (um)", size = 'xx-large')
 axes.set_ylabel("Y (um)", size = 'xx-large')
 
-x_bound = (numpy.abs(pos_table.x).max() // 1000 + 1) * 1000
-y_bound = (numpy.abs(pos_table.y).max() // 1000 + 1) * 1000
-axes.set_xlim(x_bound, -x_bound)
-axes.set_ylim(y_bound, -y_bound)
+# set the direction of axis
+x_min = (numpy.min(pos_table.x) // 1000) * 1000
+x_max = (numpy.max(pos_table.x) // 1000 + 1) * 1000
+y_min = (numpy.min(pos_table.y) // 1000) * 1000
+y_max = (numpy.max(pos_table.y) // 1000 + 1) * 1000
+
+dirs = list(filter(lambda x: x.is_dir(), input_locations))
+czis = list(filter(lambda x: x.suffix.lower() == '.czi', input_locations))
+
+if axis_mode == 'auto':
+    if len(dirs) >= len(czis):
+        axis_mode = 'dispim'
+        print("Axis direction set to the diSPIM mode")
+    else:
+        axis_mode = 'airy'
+        print("Axis direction set to the Airyscan mode")
+
+if axis_mode == 'dispim':
+    if len(czis) > 0:
+        print("Warning: some CZI files are processed.")
+    axes.set_xlim(x_max, x_min)
+    axes.set_ylim(y_max, y_min)
+else:
+    if len(dirs) > 0:
+        print("Warning: some diSPIM files are processed.")
+    axes.set_xlim(x_min, x_max)
+    axes.set_ylim(y_max, y_min)
+
 axes.axhline(0, color = 'black', linestyle = ':')
 axes.axvline(0, color = 'black', linestyle = ':')
 
-axes.scatter(pos_table.x, pos_table.y, c = pos_table.index)
 for index, item in pos_table.iterrows():
-    axes.annotate(index, (item['x'], item['y']))
+    axes.plot(item['x'], item['y'], marker = 'o', label = item['name'])
+    #axes.annotate(index, (item['x'], item['y']))
+
+axes.legend()
 figure.savefig(output_graph_filename, dpi = 300)
