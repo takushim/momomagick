27a2b02f47a7bfa1fc5565d33a7fbd4b332c249e
Takushi Miyoshi <takushi.miyoshi@nih.gov>
2021-10-03 11:47:09 -0400

    Implemented specification of z and t ranges

--- mmcrop.py
+++ mmcrop.py
@@ -12,6 +12,8 @@ use_channel = 0
 use_area = None
 preset_area_index = None
 preset_areas = mmtiff.preset_areas
+zstack_range = None
+time_range = None
 
 # parse arguments
 parser = argparse.ArgumentParser(description='Crop a diSPIM image for viewing via network. Crop using all preset areas by default.', \
@@ -30,6 +32,14 @@ group.add_argument('-R', '--use-area', type = int, nargs = 4, default = use_area
                    metavar = ('X', 'Y', 'W', "H"),
                    help='Crop using the specified area.')
 
+parser.add_argument('-Z', '--zstack-range', type = int, nargs = 2, default = zstack_range, \
+                    metavar = ('START', 'END'),
+                    help='Specify the range of z planes to output')
+
+parser.add_argument('-T', '--time-range', type = int, nargs = 2, default = time_range, \
+                    metavar = ('START', 'END'),
+                    help='Specify the range of time frames to output')
+
 parser.add_argument('input_file', default = input_filename, \
                     help='input (multipage) TIFF file')
 args = parser.parse_args()
@@ -37,6 +47,8 @@ args = parser.parse_args()
 # set arguments
 input_filename = args.input_file
 use_channel = args.use_channel
+zstack_range = args.zstack_range
+time_range = args.time_range
 
 if args.use_area is not None:
     crop_areas = [args.use_area]
@@ -61,15 +73,27 @@ else:
 input_tiff = mmtiff.MMTiff(input_filename)
 input_image = input_tiff.as_array()
 
+if zstack_range is None:
+    zstack_slice = slice(0, input_tiff.total_zstack, 1)
+else:
+    zstack_slice = slice(zstack_range[0], zstack_range[1] + 1, 1)
+
+if time_range is None:
+    time_slice = slice(0, input_tiff.total_time, 1)
+else:
+    time_slice = slice(time_range[0], time_range[1] + 1, 1)
+
+if use_channel is None:
+    channel_slice = slice(0, input_tiff.total_channel, 1)
+else:
+    channel_slice = slice(use_channel, use_channel + 1, 1)
+
 # output TIFF
 for index in range(len(crop_areas)):
     print("Cropping using area:", crop_areas[index])
-    slice_x, slice_y = mmtiff.area_to_slice(crop_areas[index])
+    x_slice, y_slice = mmtiff.area_to_slice(crop_areas[index])
     print("Output image:", output_filenames[index])
-    if use_channel is None:
-        output_image = input_image[..., slice_y, slice_x]
-    else:
-        output_image = input_image[..., use_channel:(use_channel + 1), slice_y, slice_x]
+    output_image = input_image[time_slice, zstack_slice, channel_slice, y_slice, x_slice]
 
     input_tiff.save_image(output_filenames[index], output_image)
     print(".")
