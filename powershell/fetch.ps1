7599fe421e81133d31ff36c3cdb00c70013abd6d
Takushi Miyoshi <tmiyoshi@nih.gov>
2022-01-17 13:09:42 -0500

    Improved the fetch script

--- powershell/fetch.ps1
+++ powershell/fetch.ps1
@@ -14,20 +14,37 @@ $logfile = 'fetch.log'
 
 # select a file if $file is a folder name
 if ((get-item $file) -is [System.IO.DirectoryInfo]) {
-      $glob = [IO.Path]::Combine($file, "*_crop_1_deconv.tif")
-      $file = (get-item $glob)
+      $glob = [IO.Path]::Combine($file, "*_crop_1_deconv_res.tif")
+      if (Test-Path $glob) {
+            $file = (get-item $glob)
+      }
+      else {
+            $glob = [IO.Path]::Combine($file, "*_crop_1_deconv.tif")
+            if (Test-Path $glob) {
+                  $file = (get-item $glob)
+            }
+            else{
+                  $glob = [IO.Path]::Combine($file, "*_crop_1.tif")
+                  $file = (get-item $glob)
+            }
+      }
 }
 
 $image = $file
 $folder = (get-item $image).DirectoryName
 $date = ((get-item $folder).Parent.Parent.Name -split "_")[0]
+$suffix = ((get-item $folder).Parent.Name -split "_")[-1]
 $record = [IO.Path]::Combine($folder, ("{0}_track.json" -f (get-item $image).basename))
+
+if (((get-item $folder).Name -split "_")[-1] -match "[0-9]") {
+      $counter = "_{0}" -f ((get-item $folder).Name -split "_")[-1]
+}
 
 # start logging
 Write-Output ("* {0}: {1}" -f (get-date), $folder) | Tee-object -Append -FilePath $logfile
 
 # copy image
-$output_image = "{0}_{1}" -f $date, (get-item $image).Name
+$output_image = "{0}_{1}{2}_{3}" -f $date, $suffix, $counter, (get-item $image).Name
 Write-Output ("-- {0}" -f $image) | Tee-object -Append -FilePath $logfile
 Copy-Item $image $output_image
 
