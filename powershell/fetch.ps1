30cde7b0320ed2405c80f2f6473a301165c1d3c9
Takushi Miyoshi <tmiyoshi@nih.gov>
2022-01-22 12:31:49 -0500

    Bug fix in generating the output filename

--- powershell/fetch.ps1
+++ powershell/fetch.ps1
@@ -13,6 +13,9 @@ $logfile = 'fetch.log'
 . $HOME\.venv\gpu\Scripts\Activate.ps1
 
 # select a file if $file is a folder name
+$glob = @("*_crop_1_deconv_res.tif", "*_crop_1_deconv_iso.tif", "*_crop_1_deconv.tif", "*_crop_1.tif")
+
+
 if ((get-item $file) -is [System.IO.DirectoryInfo]) {
       $glob = [IO.Path]::Combine($file, "*_crop_1_deconv_res.tif")
       if (Test-Path $glob) {
@@ -34,7 +37,7 @@ $image = $file
 $folder = (get-item $image).DirectoryName
 $date = ((get-item $folder).Parent.Parent.Name -split "_")[0]
 $cellid = ((get-item $folder).Parent.Name -split "_")[-1]
-$image_name = (get-item $image).Name
+$image_name = (get-item $image).BaseName
 $image_ext = (get-item $image).Extension
 #$record = [IO.Path]::Combine($folder, ("{0}_track.json" -f (get-item $image).basename))
 
@@ -49,7 +52,7 @@ else{
 Write-Output ("* {0}: {1}" -f (get-date), $folder) | Tee-object -Append -FilePath $logfile
 
 # copy image
-$output_image = "{0}_{1}_{2}_{3}.{4}" -f $date, $cellid, $image_name, $counter, $image_ext
+$output_image = "{0}_{1}_{2}_{3}{4}" -f $date, $cellid, $image_name, $counter, $image_ext
 Write-Output ("-- {0}" -f $image) | Tee-object -Append -FilePath $logfile
 Copy-Item $image $output_image
 
