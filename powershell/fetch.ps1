2ac0eb35ddec5c5b592ac8234decb7fbc5d0a4cd
Takushi Miyoshi <tmiyoshi@nih.gov>
2022-02-19 11:04:32 -0500

    Improved the fetching script to handle multiple files

--- powershell/fetch.ps1
+++ powershell/fetch.ps1
@@ -1,9 +1,9 @@
 # get/set parameters from command line arguments
 Param([Switch]$help = $false,
-      [String]$file = "")
+      [String[]]$folders)
 
-if (($help -eq $true) -or ($file.length -eq 0)) {
-      Write-Host ("Example: {0} -file IMAGE_FILE" -f $MyInvocation.MyCommand.Name)
+if (($help -eq $true) -or ($file.folder -eq 0)) {
+      Write-Host ("Example: {0} -folder FOLDER_LIST" -f $MyInvocation.MyCommand.Name)
       return 0
 }
 
@@ -13,49 +13,64 @@ $logfile = 'fetch.log'
 . $HOME\.venv\gpu\Scripts\Activate.ps1
 
 # select a file if $file is a folder name
-$glob = @("*_crop_1_deconv_res.tif", "*_crop_1_deconv_iso.tif", "*_crop_1_deconv.tif", "*_crop_1.tif")
+$globs = @(
+           "*poc_crop_1_deconv_res.tif",
+           "*poc_crop_1_deconv_iso.tif",
+           "*poc_crop_1_deconv.tif",
+           "*poc_crop_1.tif",
+           "*drift_crop_1_deconv_res.tif",
+           "*drift_crop_1_deconv_iso.tif",
+           "*drift_crop_1_deconv.tif",
+           "*drift_crop_1.tif",
+           "*_crop_1_deconv_res.tif",
+           "*_crop_1_deconv_iso.tif",
+           "*_crop_1_deconv.tif",
+           "*_crop_1.tif"
+           )
 
+# expand folders
+$folders = (get-item $folders)
 
-if ((get-item $file) -is [System.IO.DirectoryInfo]) {
-      $glob = [IO.Path]::Combine($file, "*_crop_1_deconv_res.tif")
-      if (Test-Path $glob) {
-            $file = (get-item $glob)
+foreach ($folder in $folders) {
+      if ((get-item $folder) -isnot [System.IO.DirectoryInfo]) {
+            Write-Host ("{0} is not a folder. Skipping." -f $folder)
+            continue
       }
-      else {
-            $glob = [IO.Path]::Combine($file, "*_crop_1_deconv.tif")
-            if (Test-Path $glob) {
-                  $file = (get-item $glob)
-            }
-            else{
-                  $glob = [IO.Path]::Combine($file, "*_crop_1.tif")
-                  $file = (get-item $glob)
+
+      foreach ($glob in $globs) {
+            $glob_path = [IO.Path]::Combine($folder, $glob)
+            if (Test-Path $glob_path) {
+                  $file = (get-item $glob_path)
+                  break
             }
       }
+
+      $image = $file
+      $folder = (get-item $image).DirectoryName
+      $date = ((get-item $folder).Parent.Parent.Name -split "_")[0]
+      $cellid = ((get-item $folder).Parent.Name -split "_")[-1]
+      $image_name = (get-item $image).BaseName
+      $image_ext = (get-item $image).Extension
+      
+      if (((get-item $folder).Name -split "_")[-1] -match "^[0-9]+\w?$") {
+            $counter = ((get-item $folder).Name -split "_")[-1]
+      }
+      else{
+            $counter = 0
+      }
+      
+      # start logging
+      Write-Output ("* {0}: {1}" -f (get-date), $folder) | Tee-object -Append -FilePath $logfile
+      
+      # copy image
+      $output_image = "{0}_{1}_{2}_{3}{4}" -f $date, $cellid, $image_name, $counter, $image_ext
+      Write-Output ("-- {0}" -f $image) | Tee-object -Append -FilePath $logfile
+      Copy-Item $image $output_image
+      
+      # end logging
+      Write-Output (".") | Tee-object -Append -FilePath $logfile
 }
 
-$image = $file
-$folder = (get-item $image).DirectoryName
-$date = ((get-item $folder).Parent.Parent.Name -split "_")[0]
-$cellid = ((get-item $folder).Parent.Name -split "_")[-1]
-$image_name = (get-item $image).BaseName
-$image_ext = (get-item $image).Extension
-#$record = [IO.Path]::Combine($folder, ("{0}_track.json" -f (get-item $image).basename))
 
-if (((get-item $folder).Name -split "_")[-1] -match "^[0-9]+\w?$") {
-      $counter = ((get-item $folder).Name -split "_")[-1]
-}
-else{
-      $counter = 0
-}
 
-# start logging
-Write-Output ("* {0}: {1}" -f (get-date), $folder) | Tee-object -Append -FilePath $logfile
-
-# copy image
-$output_image = "{0}_{1}_{2}_{3}{4}" -f $date, $cellid, $image_name, $counter, $image_ext
-Write-Output ("-- {0}" -f $image) | Tee-object -Append -FilePath $logfile
-Copy-Item $image $output_image
-
-# end logging
-Write-Output (".") | Tee-object -Append -FilePath $logfile
 
