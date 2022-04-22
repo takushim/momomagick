2b9abac8ef7053028c2520f4ac293cdba472cf28
Takushi Miyoshi <tmiyoshi@nih.gov>
2022-04-22 16:58:56 -0400

    Changed to draw lifetime always

--- mmmark.py
+++ mmmark.py
@@ -127,9 +127,8 @@ spot_list = particles.parse_tree(particles.load_spots(record_filename))
 
 # add lifetime as text
 for spot in spot_list:
-    if spot['parent'] is None:
-        count = len([s for s in spot_list if s['track'] == spot['track']])
-        spot['text'] = str(count)
+    count = len([s for s in spot_list if s['track'] == spot['track']])
+    spot['text'] = str(count)
 
 # scale spot coordinates if necessary
 if np.isclose(spot_scaling, 1.0) == False:
